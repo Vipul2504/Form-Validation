a44568bed57b22e597da416f1bf67b6bcc7264d0
Vipul Vishwakarma <pawanvishwakarma521@gmail.com>
2022-07-21 22:53:24 +0530

    done

--- Menu-slider & modal/script.js
+++ Menu-slider & modal/script.js
@@ -1,1 +1,18 @@
-const pawan = vipi;
+const toggle = document.getElementById('toggle')
+const Open = document.getElementById('open')
+const Close = document.getElementById('close')
+const modal = document.getElementById('modal')
+
+toggle.addEventListener('click',() => {
+    document.body.classList.toggle('show-nav')
+})
+
+Open.addEventListener('click',()=>{
+    modal.classList.add('show-modal')
+})
+
+Close.addEventListener('click', () =>{
+    modal.classList.remove('show-modal')
+})
+
+window.addEventListener('click', e => e.target === modal ? modal.classList.remove('show-modal'): false)
